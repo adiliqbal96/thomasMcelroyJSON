7547593fcbc66b70549ef4357ccf0a191d80ba70
adiliqbal96 <92866917+adiliqbal96@users.noreply.github.com>
2023-03-30 13:22:15 +0200

    Soutparkapp
    Southpark app

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
-"use strict";
-
-window.addEventListener("load", initApp);
+const charactersDataURL =
+  "https://raw.githubusercontent.com/cederdorff/dat-js/main/05-data/southpark.json?fbclid=IwAR3S54uHsp2dJ-XDuYYeGUxBaO-xhoXwz0ONTK-4O0aXJdHxIFkJ666VEIk";
 
 async function initApp() {
-  const thomas = await getCharacter("thomasMcelroy.json");
+  try {
+    const characters = await fetchCharactersData(charactersDataURL);
+    for (const character of characters) {
+      showCharacter(character);
+    }
 
-  showCharacter(thomas);
-  showCharacter(thomas);
-  showCharacter(thomas);
-  showCharacter(thomas);
-  showCharacter(thomas);
-  showCharacter(thomas);
-  showCharacter(thomas);
-  showCharacter(thomas);
-  showCharacter(thomas);
+    // Add event listener for sort dropdown
+    document.getElementById("sort").addEventListener("change", (e) => {
+      const sortedCharacters = sortCharacters(characters, e.target.value);
+      displayCharacters(sortedCharacters);
+    });
+  } catch (error) {
+    console.error("Error fetching characters data:", error);
+  }
+}
+
+async function fetchCharactersData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+function displayCharacters(characters) {
+  const charactersContainer = document.querySelector("#characters");
+  charactersContainer.innerHTML = "";
+  for (const character of characters) {
+    showCharacter(character);
+  }
+}
+
+function sortCharacters(characters, sortBy) {
+  const sortedCharacters = [...characters];
+  sortedCharacters.sort((a, b) => {
+    if (a[sortBy] < b[sortBy]) {
+      return -1;
+    }
+    if (a[sortBy] > b[sortBy]) {
+      return 1;
+    }
+    return 0;
+  });
+  return sortedCharacters;
 }
 
 function showCharacter(character) {
@@ -27,7 +57,7 @@ function showCharacter(character) {
           <h2>${character.name}</h2>
           <p>Ocupation: ${character.occupation}</p>
           <p>Age: ${character.age}</p>
-          <p>Voice by: ${character.voicedBy}</p>
+          <p>Voiced by: ${character.voicedBy}</p>
         </article>
   `;
   document
@@ -36,39 +66,53 @@ function showCharacter(character) {
 
   document
     .querySelector("#characters article:last-child")
-    .addEventListener("click", showDetails);
-
-  function showDetails() {
-    console.log("Show details");
-    console.log(character);
-    document.querySelector("#dialog-name").textContent = character.name;
-    document.querySelector("#dialog-nickname").textContent = character.nickname;
-    document.querySelector("#dialog-ocupation").textContent =
-      character.occupation;
-    document.querySelector("#dialog-age").textContent = character.age;
-    document.querySelector("#dialog-voicedby").textContent = character.voicedBy;
-    document.querySelector("#dialog-gender").textContent = character.gender;
-    document.querySelector("#dialog-religion").textContent = character.religion;
-    document.querySelector("#dialog-catchphrase").textContent =
-      character.catchPhrase;
-    document.querySelector("#dialog-haircolor").textContent =
-      character.hairColor;
-    document.querySelector("#dialog-schoolgrade").textContent =
-      character.schoolGrade;
-    document.querySelector("#dialog-episodes").textContent = character.episodes;
-    document.querySelector("#dialog-appearances").textContent =
-      character.appearances;
-    document.querySelector("#dialog-firstAppearance").textContent =
-      character.firstAppearance;
-    document.querySelector("#dialog-img").src = character.image;
-    document.querySelector("#dialog-character").showModal();
-  }
+    .addEventListener("click", () => showDetails(character)); // Pass character object to showDetails()
 }
 
-async function getCharacter(url) {
-  console.log("Get Character");
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-  return data;
+function showDetails(character) {
+  console.log("Show details");
+  console.log(character);
+  document.querySelector("#dialog-name").textContent = character.name;
+  document.querySelector("#dialog-nickname").textContent = character.nickname;
+  document.querySelector("#dialog-ocupation").textContent =
+    character.occupation;
+  document.querySelector("#dialog-age").textContent = character.age;
+  document.querySelector("#dialog-voicedby").textContent = character.voicedBy;
+  document.querySelector("#dialog-gender").textContent = character.gender;
+  document.querySelector("#dialog-religion").textContent = character.religion;
+  document.querySelector("#dialog-catchphrase").textContent =
+    character.catchPhrase;
+  document.querySelector("#dialog-haircolor").textContent = character.hairColor;
+  document.querySelector("#dialog-schoolgrade").textContent =
+    character.schoolGrade;
+  document.querySelector("#dialog-episodes").textContent = character.episodes;
+  document.querySelector("#dialog-appearances").textContent =
+    character.appearances;
+  document.querySelector("#dialog-firstAppearance").textContent =
+    character.firstAppearance;
+  document.querySelector("#dialog-img").src = character.image;
+  const dialog = document.querySelector("#dialog-character");
+  fadeIn(dialog);
+  dialog.querySelector(".close").addEventListener("click", () => {
+    fadeOut(dialog);
+  });
 }
+
+function fadeIn(element) {
+  element.style.opacity = "0";
+  element.style.display = "block";
+  setTimeout(() => {
+    element.style.transition = "opacity 0.5s";
+    element.style.opacity = "1";
+  }, 0);
+}
+
+function fadeOut(element) {
+  element.style.transition = "opacity 0.5s";
+  element.style.opacity = "0";
+  setTimeout(() => {
+    element.style.display = "none";
+  }, 500);
+}
+
+document.addEventListener("DOMContentLoaded", initApp);
